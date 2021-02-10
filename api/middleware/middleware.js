3f1f4328c554f659abd2eb9ef32f5b490b674626
Michelle Chen <mchen0062@gmail.com>
2021-02-10 12:54:56 -0800

    created middleware functions

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,20 +1,43 @@
-const Posts = require('../posts/posts-model'); 
 const Users = require('../users/users-model');
 
 function logger(req, res, next) {
-  // do your magic!
+  console.log(res.method, req.baseUrl, Date.now())
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // do your magic!
+const validateUserId = async (req, res, next) => {
+  const {id} = req.params
+  try {
+    const user = await Users.getById(id)
+    if(!user){
+      res.status(404).json({message: "user not found"})
+    } else {
+      req.user = user
+      next()
+    }
+  }catch(e){
+    res.status(500).json(`Server error : ${e}`)
+  }
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+if(!res.body){
+  res.status(400).json({message: "missing user data"})
+} else if (!res.body.name){
+  res.status(400).json({message: "missing required name field"})
+} else {
+  next()
+}
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!res.body){
+    res.status(400).json({message: "missing post data"})
+  } else if (!res.body.text){
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    next()
+  }
 }
 
 module.exports = {
